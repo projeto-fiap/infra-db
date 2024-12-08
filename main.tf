9f1de6c3d78610845d77f67ddc0a0191c4b12f28
GVRO2 <141280423+GVRO2@users.noreply.github.com>
2024-12-07 22:24:18 -0300

    mudança do identificador

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_db_instance" "postgres_instance" {
   allocated_storage    = 20
   engine               = "postgres"
   engine_version       = "17"
-  identifier = "my-postgres-instance"
+  identifier = "projeto_fiap"
   instance_class       = "db.t3.micro"
   username             = "postgres_user"
   password             = "mypassword"
