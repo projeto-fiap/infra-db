1f1c8bdafdc3bad1b4f9c823a4c465930aac00c3
GVRO2 <141280423+GVRO2@users.noreply.github.com>
2024-12-07 21:32:08 -0300

    add main tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_db_instance" "postgres_instance" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "17"
+  instance_class       = "db.t3.micro"
+  username             = "postgres_user"
+  password             = "mypassword"
+  skip_final_snapshot  = true
+}
