c12646b0d653dc1b86ab68f559fab160ec752a19
GVRO2 <141280423+GVRO2@users.noreply.github.com>
2024-12-07 22:21:36 -0300

    teste branch protegida

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_db_instance" "postgres_instance" {
   allocated_storage    = 20
   engine               = "postgres"
   engine_version       = "17"
+  identifier = "my-postgres-instance"
   instance_class       = "db.t3.micro"
   username             = "postgres_user"
   password             = "mypassword"
