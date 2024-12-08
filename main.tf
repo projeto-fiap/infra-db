1bbdc0300652cb1277e7d6346c8df50d0486c8fa
GVRO2 <141280423+GVRO2@users.noreply.github.com>
2024-12-08 14:32:30 -0300

    adcionando vpc no banco de dados

--- main.tf
+++ main.tf
@@ -2,6 +2,26 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_security_group" "db_sg" {
+  name        = "db_sg"
+  description = "Grupo de segurança para o banco de dados PostgreSQL"
+  vpc_id      = "vpc-banco-postgres"
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_db_instance" "postgres_instance" {
   allocated_storage    = 20
   engine               = "postgres"
@@ -11,4 +31,6 @@ resource "aws_db_instance" "postgres_instance" {
   username             = "postgres_user"
   password             = "mypassword"
   skip_final_snapshot  = true
+
+  vpc_security_group_ids = [aws_security_group.db_sg.id] # Associa o grupo de segurança
 }
