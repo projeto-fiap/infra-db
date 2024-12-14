7ad5a4c5e2eaa6902ad1a2cf8389e72ddf09a389
GVRO2 <141280423+GVRO2@users.noreply.github.com>
2024-12-14 19:49:45 -0300

    add vpc group eks

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "postgres_instance" {
   allocated_storage    = 20
   engine               = "postgres"
   engine_version       = "17"
-  identifier           = "projetofiap"
+  identifier           = "projetofiap1"
   db_name = "projetofiap"
   instance_class       = "db.t3.micro"
   username             = "postgres_user"
